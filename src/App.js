06029827248ddd8cae586ffec710fa32c5ca1b76
lawass1864 <amwass4@hotmail.com>
2024-04-20 13:22:24 +0200

    fix avec npm install react-scripts@latest --legacy-peer-deps

--- src/App.js
+++ src/App.js
@@ -1,24 +1,9 @@
-import React, { useState } from 'react';
-import SearchInput from './components/SearchInput';
-import DateInput from './components/DateInput';
-import SearchResultsList from './components/SearchResultList';
+import React from 'react';
 
 const App = () => {
-  const [searchResults, setSearchResults] = useState([]);
-
-  const handleSearch = (searchTerm) => {
-    // Simuler une recherche avec la date actuelle
-    const currentDate = new Date().toLocaleDateString();
-    const newSearchResult = { date: currentDate, content: searchTerm };
-    setSearchResults([...searchResults, newSearchResult]);
-  };
-
   return (
     <div>
-      <h1>Plan de voyage</h1>
-      <SearchInput onSearch={handleSearch} />
-
-      <SearchResultsList results={searchResults} />
+      <h1>App</h1>
     </div>
   );
 };
