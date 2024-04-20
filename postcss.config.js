af3ac1fd80fea8a02f7676b988598ceafb4ca2fd
lawass1864 <amwass4@hotmail.com>
2024-04-20 14:08:01 +0200

    npm install ok

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    plugins: [
+      require('tailwindcss'), // Importe Tailwind CSS
+      require('autoprefixer'), // Ajoute automatiquement les préfixes vendeur aux styles CSS
+    ],
+  }
+  
--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+import Plan from './pages/Plan';
+import Details from './pages/Details';
 
 const App = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    // relier toutes les pages du site
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/plan' element={<Plan />} />
+        <Route path='/details' element={<Details />} />
+    </Routes>
+    </BrowserRouter>
   );
+
 };
 
 export default App;
