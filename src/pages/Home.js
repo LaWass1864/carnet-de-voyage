4b09b81329a8b59943e9f70f11793a4ddcf2cc34
lawass1864 <amwass4@hotmail.com>
2024-04-22 01:05:25 +0200

    application ok, je deploie sur netlify

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,22 +1,38 @@
-import React, { useEffect, useState } from 'react';
-import data from '../data.json';
+import React, { useState, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import data from '../data.json'; // Importez les données depuis le fichier data.json
 import Navigation from '../components/Navigation';
 
 const Home = () => {
-  const { informations_vol, locations_de_voiture, utilisateurs } = data;
-  const [firstName, setFirstName] = useState('');
+  const [informationsVol, setInformationsVol] = useState([]);
+  const [locationVoiture, setLocationVoiture] = useState({});
+  const [prenomUtilisateur, setPrenomUtilisateur] = useState('');
+  const [numeroSiege, setNumeroSiege] = useState('');
+  const navigate = useNavigate();
 
   useEffect(() => {
-    // Récupérer l'ID de l'utilisateur connecté depuis le stockage local
+    // Récupérer l'ID de l'utilisateur connecté à partir du stockage local
     const userId = localStorage.getItem('userId');
-
-    // Trouver l'entrée correspondante dans les données utilisateur
-    const connectedUser = utilisateurs.find(user => user.id === userId);
-
-    // Récupérer le prénom de l'utilisateur connecté
-    if (connectedUser) {
-      setFirstName(connectedUser.prenom_utilisateur);
+    if (userId) {
+      // Rechercher les informations de l'utilisateur dans les données importées
+      const utilisateur = data.utilisateurs.find(utilisateur => utilisateur.id === userId);
+      if (utilisateur) {
+        setPrenomUtilisateur(utilisateur.prenom_utilisateur);
+        setNumeroSiege(utilisateur.numero_siege);
+      }
+    } else {
+      // Rediriger l'utilisateur vers la page de connexion si aucun ID d'utilisateur n'est trouvé
+      navigate('/login');
     }
+  }, [navigate]);
+
+  useEffect(() => {
+    const { informations_vol, locations_de_voiture } = data;
+
+    // Mettre à jour les états avec les informations pertinentes
+    setInformationsVol(informations_vol);
+    setLocationVoiture(locations_de_voiture[0]); // Supposant qu'il y ait toujours une seule location de voiture pour chaque utilisateur
+
   }, []);
 
   const capitalizeFirstLetter = (string) => {
@@ -26,44 +42,40 @@ const Home = () => {
   return (
     <>
       <Navigation />
-      <div className="min-h-screen bg-gray-100 flex justify-center items-center">
-        <div className="max-w-3xl w-full p-6">
-          {/* Titre avec le prénom de l'utilisateur */}
-          <h1 className="text-2xl font-bold mb-4">Hello, {firstName}</h1>
-          
-          {/* Informations du Vol */}
-          <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
-            <h2 className="text-lg font-semibold mb-4">Informations du Vol</h2>
-            {informations_vol.map((vol) => (
-              <div key={vol.id} className="mb-4">
-                {Object.entries(vol)
-                  .filter(([key]) => key !== 'id')
-                  .map(([key, value]) => (
-                    <p key={key}>
-                      <span className="font-semibold">{capitalizeFirstLetter(key.replace(/_/g, ' '))} :</span> {capitalizeFirstLetter(value)}
-                    </p>
-                  ))}
-              </div>
-            ))}
-          </div>
+    <div className="min-h-screen bg-gray-100 flex justify-center items-center">
+      <div className="max-w-3xl w-full p-6">
+        <h1 className="text-2xl font-semibold mb-6">Hello, {prenomUtilisateur}</h1>
+        {/* Informations du Vol */}
+        <div className="bg-white shadow-lg rounded-lg p-6 mb-6">
+          <h2 className="text-lg font-semibold mb-4">Informations du Vol</h2>
+          {informationsVol.map((vol) => (
+            <div key={vol.id} className="mb-4">
+              {Object.entries(vol)
+                .filter(([key]) => key !== 'id')
+                .map(([key, value]) => (
+                  <p key={key}>
+                    <span className="font-semibold">{capitalizeFirstLetter(key.replace(/_/g, ' '))} :</span> {capitalizeFirstLetter(value)}
+                  </p>
+                ))}
+            </div>
+          ))}
+          {/* Affichage du numéro de siège */}
+          <p><strong>Numéro de siège :</strong> {numeroSiege}</p>
+        </div>
 
-          {/* Location de Voiture */}
-          <div className="bg-white shadow-lg rounded-lg p-6">
-            <h2 className="text-lg font-semibold mb-4">Location de Voiture</h2>
-            {locations_de_voiture.map((location) => (
-              <div key={location.id} className="mb-4">
-                {Object.entries(location)
-                  .filter(([key]) => key !== 'id')
-                  .map(([key, value]) => (
-                    <p key={key}>
-                      <span className="font-semibold">{capitalizeFirstLetter(key.replace(/_/g, ' '))} :</span> {capitalizeFirstLetter(value)}
-                    </p>
-                  ))}
-              </div>
+        {/* Location de Voiture */}
+        <div className="bg-white shadow-lg rounded-lg p-6">
+          <h2 className="text-lg font-semibold mb-4">Location de Voiture</h2>
+          {Object.entries(locationVoiture)
+            .filter(([key]) => key !== 'id')
+            .map(([key, value]) => (
+              <p key={key}>
+                <span className="font-semibold">{capitalizeFirstLetter(key.replace(/_/g, ' '))} :</span> {capitalizeFirstLetter(value)}
+              </p>
             ))}
-          </div>
         </div>
       </div>
+    </div>
     </>
   );
 };
