d58bcab092b9b00fc2f01b2123852d32d4cb172f
lawass1864 <amwass4@hotmail.com>
2024-04-22 01:56:19 +0200

    modification page app.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,21 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Plan from './pages/Plan';
 import Details from './pages/Details';
 import Login from './pages/Login';
 
 const App = () => {
+  // Vérifiez ici s'il y a une session active ou non
+  const isAuthenticated = false; // Remplacez par la logique réelle de vérification de l'authentification
+
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/login" element={<Login />} />
+        {/* Redirige vers la page de connexion si l'utilisateur n'est pas authentifié */}
+        {!isAuthenticated && <Route path="/" element={<Navigate to="/login" />} />}
+        {/* Définition des autres routes */}
+        <Route path="/login" element={<Login />} />
         <Route path="/home/:seatNumber" element={<Home />} />
         <Route path="/plan" element={<Plan />} />
         <Route path="/details" element={<Details />} />
