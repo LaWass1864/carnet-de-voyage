4ce5bc30099f3f347c20c4e7098658ffd0ee005c
lawass1864 <amwass4@hotmail.com>
2024-05-16 11:05:49 +0200

    suppresion du fichier .env dans .gitignore

--- copy-json.js
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const path = require('path');
-
-const source = path.join(__dirname, 'src/data.json');  // Chemin vers ton fichier JSON
-const destination = path.join(__dirname, 'build/data.json');  // Chemin vers le dossier de build
-
-fs.copyFileSync(source, destination);
-console.log('data.json has been copied to the build folder');
