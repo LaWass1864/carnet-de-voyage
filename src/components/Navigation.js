4e6004ec7d46b49a088a35d53f8bdac4dac870ee
lawass1864 <amwass4@hotmail.com>
2024-05-16 12:00:09 +0200

    test nvaigation responsive

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -3,26 +3,8 @@ import React from 'react';
 const Navigation = () => {
   return (
     <nav className="mx-auto max-w-screen-md px-4 py-2 text-white bg-white border shadow-md rounded-xl border-white/80 bg-opacity-80 backdrop-blur-2xl backdrop-saturate-200">
-      <div className="flex items-center justify-between">
-        <a href="#" className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased">
-          Material Tailwind
-        </a>
-        {/* Utiliser une classe différente pour la version mobile */}
-        <ul className="hidden lg:flex gap-6">
-          <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2">
-            <a href="#" className="flex items-center">Pages</a>
-          </li>
-          <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2">
-            <a href="#" className="flex items-center">Account</a>
-          </li>
-          <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2">
-            <a href="#" className="flex items-center">Blocks</a>
-          </li>
-          <li className="flex items-center p-1 font-sans text-sm antialiased font-medium leading-normal gap-x-2">
-            <a href="#" className="flex items-center">Docs</a>
-          </li>
-        </ul>
-        <div className="flex items-center justify-center gap-x-4">
+      <div className="flex flex-col lg:flex-row items-center justify-between">
+        <div className="lg:order-2 flex items-center gap-x-4">
           <button className="px-4 py-2 font-sans text-xs font-bold text-center text-gray-900 uppercase align-middle transition-all rounded-lg select-none hover:bg-gray-900/10 active:bg-gray-900/20 disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:inline-block" type="button">
             <a href="/home/:seatNumber">Home</a>
           </button>
@@ -35,7 +17,9 @@ const Navigation = () => {
             <a href="/details">Details</a>
           </button>
         </div>
-        {/* Afficher le bouton de menu pour les appareils mobiles */}
+        <a href="#" className="mr-4 block cursor-pointer py-1.5 font-sans text-base font-medium leading-relaxed text-inherit antialiased lg:order-1">
+          Material Tailwind
+        </a>
         <button className="relative h-6 w-6 select-none rounded-lg text-center align-middle font-sans text-xs font-medium uppercase text-inherit transition-all hover:bg-transparent focus:bg-transparent active:bg-transparent disabled:pointer-events-none disabled:opacity-50 disabled:shadow-none lg:hidden" type="button">
           <span className="absolute transform -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2">
             <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" stroke="currentColor" strokeWidth="2" viewBox="0 0 24 24" strokeLinecap="round" strokeLinejoin="round">
